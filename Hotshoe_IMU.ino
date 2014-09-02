92c8bccded40a5675a633b8887090de548bc74c0
Tisham Dhar <whatnickd@gmail.com>
2014-09-02 23:42:31 +0930

    Added faster GPS parsing

--- Hotshoe_IMU.ino
+++ Hotshoe_IMU.ino
@@ -196,9 +196,9 @@ void recordData()
       
       SeeedOled.setTextXY(7,0);
       char sz[32];
-      sprintf(sz, "%02d:%02d:%02d ", t.hour(), t.minute(), t.second());
+      sprintf(sz, "%02d%02d%02d ", t.hour(), t.minute(), t.second());
       SeeedOled.putString(sz);
-      sprintf(sz, "%02d/%02d/%02d ", d.month(), d.day(), d.year());
+      sprintf(sz, "%02d%02d%02d", d.month(), d.day(), d.year());
       SeeedOled.putString(sz);
     }
 
@@ -230,6 +230,11 @@ void recordData()
     deltat = ((Now - lastUpdate)/1000000.0f); // set integration time by time elapsed since last filter update
     lastUpdate = Now;
   
+    SeeedOled.setTextXY(0,5);
+    SeeedOled.putFloat(1.0f/deltat);
+    SeeedOled.putString("Hz");
+    
+    
     int i;
     for(i=0;i<10;i++)
     // read raw accel/gyro measurements from device
@@ -247,6 +252,7 @@ void recordData()
       // Pass gyro rate as rad/s
        MadgwickQuaternionUpdate(Axyz[0], Axyz[1], Axyz[2], Gxyz[0]*PI/180.0f, Gxyz[1]*PI/180.0f, Gxyz[2]*PI/180.0f,  Mxyz[0],  Mxyz[1], Mxyz[2]);
       // MahonyQuaternionUpdate(ax, ay, az, gx*PI/180.0f, gy*PI/180.0f, gz*PI/180.0f, my, mx, mz);
+      smartDelay(0);
     }
   
     // Define output variables from updated quaternion---these are Tait-Bryan angles, commonly used in aircraft orientation.
@@ -279,10 +285,6 @@ void recordData()
       Serial.print("average rate = "); Serial.print(1.0f/deltat, 2); Serial.println(" Hz");
     }
     
-    SeeedOled.setTextXY(0,5);
-    SeeedOled.putFloat(1.0f/deltat);
-    SeeedOled.putString("Hz");
-    
     SeeedOled.setTextXY(1,0);
     SeeedOled.putString("Y=");
     SeeedOled.setTextXY(1,2);
@@ -297,6 +299,7 @@ void recordData()
     SeeedOled.putString("P=");
     SeeedOled.setTextXY(3,2);
     SeeedOled.putFloat(pitch.number);
+    
 }
 
 //=========================================
@@ -384,74 +387,4 @@ static void smartDelay(unsigned long ms)
     while (Serial1.available())
       gps.encode(Serial1.read());
   } while (millis() - start < ms);
-}
-
-static void printFloat(float val, bool valid, int len, int prec)
-{
-  if (!valid)
-  {
-    while (len-- > 1)
-      Serial.print('*');
-    Serial.print(' ');
-  }
-  else
-  {
-    Serial.print(val, prec);
-    int vi = abs((int)val);
-    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
-    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
-    for (int i=flen; i<len; ++i)
-      Serial.print(' ');
-  }
-  smartDelay(0);
-}
-
-static void printInt(unsigned long val, bool valid, int len)
-{
-  char sz[32] = "*****************";
-  if (valid)
-    sprintf(sz, "%ld", val);
-  sz[len] = 0;
-  for (int i=strlen(sz); i<len; ++i)
-    sz[i] = ' ';
-  if (len > 0) 
-    sz[len-1] = ' ';
-  Serial.print(sz);
-  smartDelay(0);
-}
-
-static void printDateTime(TinyGPSDate &d, TinyGPSTime &t)
-{
-  if (!d.isValid())
-  {
-    Serial.print(F("********** "));
-  }
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d/%02d/%02d ", d.month(), d.day(), d.year());
-    Serial.print(sz);
-  }
-  
-  if (!t.isValid())
-  {
-    Serial.print(F("******** "));
-  }
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d:%02d:%02d ", t.hour(), t.minute(), t.second());
-    Serial.print(sz);
-  }
-
-  printInt(d.age(), d.isValid(), 5);
-  smartDelay(0);
-}
-
-static void printStr(const char *str, int len)
-{
-  int slen = strlen(str);
-  for (int i=0; i<len; ++i)
-    Serial.print(i<slen ? str[i] : ' ');
-  smartDelay(0);
 }
